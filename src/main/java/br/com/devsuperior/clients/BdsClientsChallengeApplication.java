77519c8b273dbf31dd8739fb3c826ba7b9d78524
Thaylor Carvalho <thaylorcs@outlook.com>
2021-03-31 15:26:43 -0300

    Started Project

---
+++ src/main/java/br/com/devsuperior/clients/BdsClientsChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsClientsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsClientsChallengeApplication.class, args);
+	}
+
+}
