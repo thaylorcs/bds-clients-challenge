23cd0cc3d246cbe9e417eec73a720d4a04b3f85a
Thaylor Carvalho <thaylorcs@outlook.com>
2021-03-31 18:53:08 -0300

    Database Seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Thaylor', '111.222.333-45', 800.0, '1991-03-19', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Gabriela', '444.555.666-78', 900.0, '1993-08-30', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Dorival', '666.777.888-90', 300.0, '1994-06-01', 0);   
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Vania', '111.111.111-22', 300.0, '1961-10-09', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Angela', '222.222.222-33', 400.0, '1998-09-03', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Yasmin', '333.333.333-44', 200.0, '1992-08-26', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('João', '444.444.444-55', 200.0, '2003-01-25', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Thiago', '555.555.555-66', 1000.0, '1985-02-07', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Nelio', '666.666.666-77', 10000.0, '1975-02-07', 0);
+INSERT into tb_client (name, cpf, income, birth_date, children) VALUES ('Washington', '777.777.777-88', 10000.0, '1995-02-07', 0);
