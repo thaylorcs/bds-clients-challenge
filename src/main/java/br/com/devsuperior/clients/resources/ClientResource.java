e0e535dc8035215d253c8a79cc7724ff0b77aec9
Thaylor Carvalho <thaylorcs@outlook.com>
2021-04-06 16:14:56 -0300

    POST ok

--- src/main/java/br/com/devsuperior/clients/resources/ClientResource.java
+++ src/main/java/br/com/devsuperior/clients/resources/ClientResource.java
@@ -1,5 +1,7 @@
 package br.com.devsuperior.clients.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,9 +9,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.devsuperior.clients.dto.ClientDTO;
 import br.com.devsuperior.clients.services.ClientService;
@@ -42,5 +47,15 @@ public class ClientResource {
 
 		ClientDTO dto = service.findById(id);		
 		return ResponseEntity.ok().body(dto);		
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert (@RequestBody ClientDTO dto){
+		
+		ClientDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(newDto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(newDto);
 	}
 }
