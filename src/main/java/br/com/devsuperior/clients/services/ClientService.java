55aba13ab3ca244d1f899d6e7c8abddead09fe76
Thaylor Carvalho <thaylorcs@outlook.com>
2021-04-06 16:42:07 -0300

    PUT ok

--- src/main/java/br/com/devsuperior/clients/services/ClientService.java
+++ src/main/java/br/com/devsuperior/clients/services/ClientService.java
@@ -2,6 +2,8 @@ package br.com.devsuperior.clients.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -40,14 +42,24 @@ public class ClientService {
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity); 
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
 
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
-		entity.setChildren(dto.getChildren());
-		
-		
+		entity.setChildren(dto.getChildren());		
 	}
 }
